8bd9c20d166ab7671b14c4a624aa65ae498ace3b
Sara Mota <saraamotalima@gmail.com>
2023-01-12 14:43:28 -0300

    index

--- exemplo-caneta/index.php
+++ exemplo-caneta/index.php
@@ -5,10 +5,17 @@ $c1 = new Caneta;
 $c1->cor = "Azul";
 $c1->ponta = 0.5;
 $c1->tampada = false;
-
 $c1->destampar();
 $c1->rabiscar();
+print_r($c1);
+echo "<br/>";
 
-//print_r($c1);
+$c2 = new Caneta;
+$c2->cor = "Verde";
+$c2->carga = 50;
+$c2->tampada = false;
+$c2->tampar();
+$c2->rabiscar();
+print_r($c2);
 
 ?>
